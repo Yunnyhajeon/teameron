97d86392aa614abc18ed3dc5525bbd35a6bc5d5a
Xiangling Tham <xtham3@gatech.edu>
2020-01-23 06:11:24 -0500

    fixed toolbar with webview

--- app/src/main/java/com/example/eron/ebook.java
+++ app/src/main/java/com/example/eron/ebook.java
@@ -2,9 +2,6 @@ package com.example.eron;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import android.view.MenuItem;
 import android.view.View;
 
@@ -16,6 +13,8 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -46,13 +45,29 @@ public class ebook extends AppCompatActivity {
         setContentView(R.layout.activity_ebook);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Red Light Warning Signals");
 
         expandableListView = findViewById(R.id.expandable_list_view);
         prepareMenuData();
         populateExpandableList();
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
         NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+        /*
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -63,21 +78,59 @@ public class ebook extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+         */
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        /*
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+         */
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.ebook, menu);
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
         return true;
     }
 
+    /*
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+     */
+
+    /*
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+    */
+
 
     private void prepareMenuData() {
         /*
@@ -101,7 +154,7 @@ public class ebook extends AppCompatActivity {
         children.add(childItem);
         childItem = new MenuItemModel("Head Injury", "file:///android_asset/part1_bodyparts/Head_2_HeadInjury.html", false, false);
         children.add(childItem);
-        childItem = new MenuItemModel("Psychological Problems", "file:///android_asset/part1_bodyparts/Head_1_PsychologicalProblems.html", false, false);
+        childItem = new MenuItemModel("Psychological Problems", "file:///android_asset/part1_bodyparts/Head_3_PsychologicalProblems.html", false, false);
         children.add(childItem);
         childList.put(headerItem, children);
 
@@ -115,16 +168,25 @@ public class ebook extends AppCompatActivity {
         expandableListAdapter = new ExpandableListAdapter(this, headerList, childList);
         expandableListView.setAdapter(expandableListAdapter);
 
+
         expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
             @Override
             public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
 
                 if (headerList.get(groupPosition).isGroup) {
                     if (!headerList.get(groupPosition).hasChildren) {
-                        WebView webView = v.findViewById(R.id.menu_content_webview);
-                        Log.i("URL", headerList.get(groupPosition).htmlUrl);
-                        webView.loadUrl(headerList.get(groupPosition).htmlUrl);
-                        //onBackPressed();
+                        //setContentView(R.layout.menu_item_content);
+                        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                        drawer.closeDrawer(GravityCompat.START, false);
+                        WebView webView = (WebView) findViewById(R.id.menu_content_webview);
+
+                        if (webView == null) {
+                            Log.i("WebViewNullCheck", "WebView is NULL!!!");
+                        } else {
+                            Log.i("URL", headerList.get(groupPosition).htmlUrl);
+                            webView.loadUrl(headerList.get(groupPosition).htmlUrl);
+                            //onBackPressed();
+                        }
                     }
                 }
 
@@ -139,10 +201,19 @@ public class ebook extends AppCompatActivity {
                 if (childList.get(headerList.get(groupPosition)) != null) {
                     MenuItemModel model = childList.get(headerList.get(groupPosition)).get(childPosition);
                     if (model.htmlUrl.length() > 0) {
-                        WebView webView = v.findViewById(R.id.menu_content_webview);
-                        Log.i("URL", headerList.get(groupPosition).htmlUrl);
-                        webView.loadUrl(model.htmlUrl);
-                        //onBackPressed();
+                        //setContentView(R.layout.menu_item_content);
+                        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                        drawer.closeDrawer(GravityCompat.START, false);
+                        WebView webView = (WebView) findViewById(R.id.menu_content_webview);
+
+                        if (webView == null) {
+                            Log.i("WebViewNullCheck", "WebView is NULL!!!");
+                        } else {
+                            Log.i("URL", headerList.get(groupPosition).htmlUrl);
+                            webView.loadUrl(model.htmlUrl);
+                            //onBackPressed();
+                        }
+
                     }
                 }
 

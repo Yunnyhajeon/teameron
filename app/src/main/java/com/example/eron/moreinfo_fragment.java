d1e1fb0c209cfbbfc94a6db84c6a813c8357cd3a
Xiangling Tham <xtham3@gatech.edu>
2020-01-24 15:38:33 -0500

    add scroll to more info page

--- app/src/main/java/com/example/eron/moreinfo_fragment.java
+++ app/src/main/java/com/example/eron/moreinfo_fragment.java
@@ -1,10 +1,12 @@
 package com.example.eron;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,8 @@ public class moreinfo_fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.moreinfo_frag, container, false);
+        TextView moreInfoText = (TextView) view.findViewById(R.id.more_info_textview);
+        moreInfoText.setMovementMethod(new ScrollingMovementMethod());
         home_btn = view.findViewById(R.id.home_btn_from_moreinfo);
         home_btn.setOnClickListener(new View.OnClickListener() {
             @Override

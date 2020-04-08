0a63a0ffe66e7d6b72e8c23f9acef12393fc8dff
MuddassirBahri <mbahri3@gatech.edu>
2020-04-07 23:45:20 -0400

    changed CheckedActivity to tip_symptom_finder

--- app/src/main/java/com/example/eron/SF_activity.java
+++ app/src/main/java/com/example/eron/SF_activity.java
@@ -34,7 +34,7 @@ public class SF_activity extends AppCompatActivity {
         submit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SF_activity.this,CheckedActivity.class);
+                Intent intent = new Intent(SF_activity.this,tip_symptom_finder.class);
                 startActivity(intent);
             }
         });

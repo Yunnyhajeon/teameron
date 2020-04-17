86fd13ac1f4634ada942ad2fc4940441c6ce3174
lingtham <xling.tham@gmail.com>
2020-04-17 18:47:26 -0400

    add back and home buttons

--- app/src/main/java/com/example/eron/GuideContentFragment.java
+++ app/src/main/java/com/example/eron/GuideContentFragment.java
@@ -2,6 +2,8 @@ package com.example.eron;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -36,7 +38,21 @@ public class GuideContentFragment extends Fragment {
 
         toolbar.setTitle(getArguments().getString("title"));
 
+        toolbar.inflateMenu(R.menu.home);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.home_icon) {
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, new home_screen_fragment()).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         return view;
 
     }
+
 }
